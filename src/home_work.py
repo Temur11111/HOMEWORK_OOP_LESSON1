8029471dfbdb4441f616c0f26275e62641ab95a5
Temur Sadykow <raspopova9191@bk.ru>
2024-03-18 21:31:22 +0300

    выполнил первое задание

---
+++ src/home_work.py
@@ -0,0 +1,14 @@
+class Category:
+    name: str
+    descriptions: str
+    products: list
+
+    pass
+
+
+
+class Product:
+    name: str
+    descriptions: str
+    price: float
+    quantity in stock: int ###количество в наличии
