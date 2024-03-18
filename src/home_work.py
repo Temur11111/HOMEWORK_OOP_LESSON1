052ef1d8d561b8857e1f57de72139acf8947657f
Temur Sadykow <raspopova9191@bk.ru>
2024-03-18 21:54:23 +0300

    выполнил второе задание

--- src/home_work.py
+++ src/home_work.py
@@ -3,7 +3,11 @@ class Category:
     descriptions: str
     products: list
 
-    pass
+    def __init__(self, name, descriptions, products):
+        self.name = name
+        self.descriptions = descriptions
+        self.products = products
+
 
 
 
@@ -11,4 +15,16 @@ class Product:
     name: str
     descriptions: str
     price: float
-    quantity in stock: int ###количество в наличии
+    quantity_in_stock: int ###количество в наличии
+    counter_categories: int
+
+    counter_categories = 0
+    original_product = 0
+
+    def __init__(self, name, descriptions, price, quantity_in_stock):
+        self.name = name
+        self.descriptions = descriptions
+        self.price = price
+        self.quantity_in_stock = quantity_in_stock
+
+
