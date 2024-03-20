bec2a50e5c18950706c96bcd5f8fe6003db7cebc
Temur Sadykow <raspopova9191@bk.ru>
2024-03-20 16:58:33 +0300

    скоректировал задания и написал тесты

--- src/home_work.py
+++ src/home_work.py
@@ -1,35 +1,42 @@
 class Category:
-    name: str
-    descriptions: str
-    products: list
+    """Класс категории"""
     counter_categories: int
     original_product: int
 
-
     counter_categories = 0####количество категории
-    original_product = 0#####количество оригинальных продуктов
+    original_product = 0###количество оригинальных продуктов
+
 
 
     def __init__(self, name, descriptions, products):
+        name: str
+        descriptions: str
+        products: list
         self.name = name
         self.descriptions = descriptions
         self.products = products
-
-
+        Category.counter_categories += 1
+        if len(products) == len(set(products)):
+            Category.original_product = len(products)
+        else:
+            Category.original_product = len(set(products))
 
 
 
 
 class Product:
+    """Класс продуктов"""
     name: str
     descriptions: str
     price: float
     quantity_in_stock: int ###количество в наличии
-    counter_categories: int
-
 
 
     def __init__(self, name, descriptions, price, quantity_in_stock):
+        name: str
+        descriptions: str
+        price: float
+        quantity_in_stock: int  ###количество в наличии
         self.name = name
         self.descriptions = descriptions
         self.price = price
